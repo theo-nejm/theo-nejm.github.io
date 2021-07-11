93409372bef3386de34c21c935b6e3618a6d69da
Theo Nejm <theoonejm@gmail.com>
2021-07-11 13:24:42 -0300

    Make projects constructor function as a module and add pixelize-me

---
+++ projects.js
@@ -0,0 +1,30 @@
+const Project = function(name, description, link) {
+  this.name = name;
+  this.description = description;
+  this.link = link;
+}
+
+export const devFinances = new Project('dev.finance$',
+  "dev-finance$: este foi um projeto construído durante a maratona do Discover. Ele tem como função principal receber e armazenar internamente no browser quantias de entradas e saídas de dinheiro e calcular para devolver o balanço de forma reativa.",
+  'dev-finances'
+)
+
+export const fastapping = new Project('fastapping',
+  "fastapping: este foi um projeto que eu criei do zero. Ele é um 'game' que testa quantas palavras o usuário consegue digitar por minuto e a precisão ao digitar e, conforme o usuário vai treinando, um gráfico vai sendo desenvolvido com a evolução dele. O programa foi feito com apenas HTML, CSS e Javascript puro.",
+  'fastapping'
+)
+
+export const rocketQ = new Project("rocket.q",
+  "rocket.q: este foi um projeto desenvolvido durante a NLW - together, da Rocketseat. Ele trata de ciar ou entrar em salas já existentes para fazer perguntas anônimas, que podem ser moderadas pelo criador da sala ou qualquer um que tenha a senha dela. O programa foi feito com EJS, Javascript, Node com express e usando o SQLite como banco de dados.",
+  'https://rocketq-app.herokuapp.com/'
+)
+
+export const todoList = new Project("todo-list",
+  "todo-list: este foi um projeto desenvolvido por mim para auxiliar os alunos do IFCE a iniciarem com o desenvolvimento para a web. Ele é um CRUD de uma lista de tarefas que possui o armazenamento local e permite criar quantas tarefas forem necessárias, marcá-las como concluídas e excluí-las. Ele usa somente HTML, CSS e Javascript puro.",
+  'todo-list'
+)
+
+export const pixelizeMe = new Project("pixelize.me",
+  "todo-list: este foi um projeto desenvolvido por mim para permitir pessoas a fazerem pixel arts (desenhos feitos apenas com pixels) de maneira simples e gratuita. Ele usa somente HTML, CSS e Javascript puro.",
+  'pixelize-me'
+)
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { devFinances, rocketQ, fastapping, todoList, pixelizeMe } from './projects.js';
+
 // CAROUSEL ===========================================================
 
 const slides = document.querySelectorAll(".carousel-image");
@@ -16,183 +18,258 @@ height = Number(height)
 let currentImageIndex = 0;
 
 function forceChangeImage(currentImageIndex) {
-    slides.forEach(slide => {
-        slide.classList.remove("selected")
-    })
+  slides.forEach(slide => {
+    slide.classList.remove("selected")
+  })
 
-    slides[currentImageIndex].classList.add("selected")
+  slides[currentImageIndex].classList.add("selected")
 
-    switch (currentImageIndex) {
-        case 0:
-            projectTitle.innerText = "dev.finance$";
-            projectContent.innerText = "dev-finance$: este foi um projeto construído durante a maratona do Discover. Ele tem como função principal receber e armazenar internamente no browser quantias de entradas e saídas de dinheiro e calcular para devolver o balanço de forma reativa."
-            allProjectItems.forEach(projectItem => {
-                projectItem.classList.remove("selected-li")
-            })
-            project1.classList.add("selected-li")
-            projectLink.setAttribute('href', 'dev-finances')
-            break
-        case 1:
-            projectTitle.innerText = 'fastapping'
-            projectContent.innerText = "fastapping: este foi um projeto que eu criei do zero. Ele é um 'game' que testa quantas palavras o usuário consegue digitar por minuto e a precisão ao digitar e, conforme o usuário vai treinando, um gráfico vai sendo desenvolvido com a evolução dele. O programa foi feito com apenas HTML, CSS e Javascript puro."
-            allProjectItems.forEach(projectItem => {
-                projectItem.classList.remove("selected-li")
-            })
-            project2.classList.add("selected-li")
-            projectLink.setAttribute('href', 'fastapping')
-            break
-        case 2:
-            projectTitle.innerText = "rocket.q"
-            projectContent.innerText = "rocket.q: este foi um projeto desenvolvido durante a NLW - together, da Rocketseat. Ele trata de ciar ou entrar em salas já existentes para fazer perguntas anônimas, que podem ser moderadas pelo criador da sala ou qualquer um que tenha a senha dela. O programa foi feito com EJS, Javascript, Node com express e usando o SQLite como banco de dados."
-            allProjectItems.forEach(projectItem => {
-                projectItem.classList.remove("selected-li")
-            })
-            project3.classList.add("selected-li")
-            projectLink.setAttribute('href', 'https://rocketq-app.herokuapp.com/')
-            break
-        case 3: 
-            projectTitle.innerText = "todo-list"
-            projectContent.innerText = "todo-list: este foi um projeto desenvolvido por mim para auxiliar os alunos do IFCE a iniciarem com o desenvolvimento para a web. Ele é um CRUD de uma lista de tarefas que possui o armazenamento local e permite criar quantas tarefas forem necessárias, marcá-las como concluídas e excluí-las. Ele usa somente HTML, CSS e Javascript puro."
-            allProjectItems.forEach(projectItem => {
-                projectItem.classList.remove("selected-li")
-            })
-            project4.classList.add("selected-li")
-            projectLink.setAttribute('href', 'todo-list')
-            break
-    }
+  switch (currentImageIndex) {
+    case 0:
+      projectTitle.innerText = devFinances.name;
+      projectContent.innerText = devFinances.description;
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project1.classList.add("selected-li")
+      projectLink.setAttribute('href', devFinances.link)
+      break
+    case 1:
+      projectTitle.innerText = fastapping.name
+      projectContent.innerText = fastapping.description
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project2.classList.add("selected-li")
+      projectLink.setAttribute('href', fastapping.link)
+      break
+    case 2:
+      projectTitle.innerText = rocketQ.name
+      projectContent.innerText = rocketQ.description
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project3.classList.add("selected-li")
+      projectLink.setAttribute('href', rocketQ.link)
+      break
+    case 3:
+      projectTitle.innerText = todoList.name
+      projectContent.innerText = todoList.description
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project4.classList.add("selected-li")
+      projectLink.setAttribute('href', todoList.link)
+      break
+    case 4:
+      projectTitle.innerText = pixelizeMe.name
+      projectContent.innerText = pixelizeMe.description
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project5.classList.add("selected-li")
+      projectLink.setAttribute('href', pixelizeMe.link)
+      break
+
+  }
 }
 
+allProjectItems.forEach(projectItem => {
+  addEventListener('click', e => {
+    const el = e.target;
+    const id = el.id
+
+    switch(id) {
+      case 'project1':
+        forceChangeImage(0)
+        break
+
+      case 'project2':
+        forceChangeImage(1)
+        break
+
+      case 'project3':
+        forceChangeImage(2)
+        break
+
+      case 'project4':
+        forceChangeImage(3)
+        break
+      case 'project5':
+        forceChangeImage(4)
+        break
+    }
+  })
+})
+
+
 nextButton.addEventListener("click", () => {
-    if (currentImageIndex === slides.length - 1) {
-        currentImageIndex = 0
-    } else {
-        currentImageIndex++
-    }
+  if (currentImageIndex === slides.length - 1) {
+    currentImageIndex = 0
+  } else {
+    currentImageIndex++
+  }
 
-    slides.forEach(slide => {
-        slide.classList.remove("selected")
-    })
+  slides.forEach(slide => {
+    slide.classList.remove("selected")
+  })
 
-    slides[currentImageIndex].classList.add("selected")
+  slides[currentImageIndex].classList.add("selected")
 
-    switch (currentImageIndex) {
-        case 0:
-            projectTitle.innerText = "dev.finance$";
-            projectContent.innerText = "dev-finance$: este foi um projeto construído durante a maratona do Discover. Ele tem como função principal receber e armazenar internamente no browser quantias de entradas e saídas de dinheiro e calcular para devolver o balanço de forma reativa."
-            project4.classList.remove("selected-li")
-            project1.classList.add("selected-li")
-            projectLink.setAttribute('href', 'dev-finances')
-            break
-        case 1:
-            projectTitle.innerText = "fastapping"
-            projectContent.innerText = "fastapping: este foi um projeto que eu criei do zero. Ele é um 'game' que testa quantas palavras o usuário consegue digitar por minuto e a precisão ao digitar e, conforme o usuário vai treinando, um gráfico vai sendo desenvolvido com a evolução dele. O programa foi feito com apenas HTML, CSS e Javascript puro."
-            project1.classList.remove("selected-li")
-            project2.classList.add("selected-li")
-            projectLink.setAttribute('href', 'fastapping')
-            break
-        case 2:
-            projectTitle.innerText = "rocket.q"
-            projectContent.innerText = "rocket.q: este foi um projeto desenvolvido durante a NLW - together, da Rocketseat. Ele trata de ciar ou entrar em salas já existentes para fazer perguntas anônimas, que podem ser moderadas pelo criador da sala ou qualquer um que tenha a senha dela. O programa foi feito com EJS, Javascript, Node com express e usando o SQLite como banco de dados."
-            project2.classList.remove("selected-li")
-            project3.classList.add("selected-li")
-            projectLink.setAttribute('href', 'https://rocketq-app.herokuapp.com/')
-            break
-        case 3: 
-            projectTitle.innerText = "todo-list"
-            projectContent.innerText = "todo-list: este foi um projeto desenvolvido por mim para auxiliar os alunos do IFCE a iniciarem com o desenvolvimento para a web. Ele é um CRUD de uma lista de tarefas que possui o armazenamento local e permite criar quantas tarefas forem necessárias, marcá-las como concluídas e excluí-las. Ele usa somente HTML, CSS e Javascript puro."
-            project3.classList.remove("selected-li")
-            project4.classList.add("selected-li")
-            projectLink.setAttribute('href', 'todo-list')
-            break
-    }
+  switch (currentImageIndex) {
+    case 0:
+      projectTitle.innerText = devFinances.name
+      projectContent.innerText = devFinances.description
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project1.classList.add("selected-li")
+      projectLink.setAttribute('href', devFinances.link)
+      break
+    case 1:
+      projectTitle.innerText = fastapping.name;
+      projectContent.innerText = fastapping.description;
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project2.classList.add("selected-li")
+      projectLink.setAttribute('href', fastapping.link)
+      break
+    case 2:
+      projectTitle.innerText = rocketQ.name;
+      projectContent.innerText = rocketQ.description;
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project3.classList.add("selected-li")
+      projectLink.setAttribute('href', rocketQ.link);
+      break
+    case 3:
+      projectTitle.innerText = todoList.name;
+      projectContent.innerText = todoList.description;
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project4.classList.add("selected-li")
+      projectLink.setAttribute('href', todoList.link);
+      break
+    case 4:
+      projectTitle.innerText = pixelizeMe.name;
+      projectContent.innerText = pixelizeMe.description;
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project5.classList.add("selected-li")
+      projectLink.setAttribute('href', pixelizeMe.link);
+      break
+
+  }
 })
 
 prevButton.addEventListener("click", () => {
-    if (currentImageIndex === 0) {
-        currentImageIndex = slides.length - 1
-    } else {
-        --currentImageIndex
-    }
+  if (currentImageIndex === 0) {
+    currentImageIndex = slides.length - 1
+  } else {
+    --currentImageIndex
+  }
 
-    slides.forEach(slide => {
-        slide.classList.remove("selected")
-    })
+  slides.forEach(slide => {
+    slide.classList.remove("selected")
+  })
 
-    slides[currentImageIndex].classList.add("selected")
+  slides[currentImageIndex].classList.add("selected")
 
 
-    switch (currentImageIndex) {
-        case 0:
-            projectTitle.innerText = "dev.finance$";
-            projectContent.innerText = "dev-finance$: este foi um projeto construído durante a maratona do Discover. Ele tem como função principal receber e armazenar internamente no browser quantias de entradas e saídas de dinheiro e calcular para devolver o balanço de forma reativa. O programa foi feito apenas com HTML, CSS e Javascript puro."
-            project2.classList.remove("selected-li")
-            project1.classList.add("selected-li")
-            projectLink.setAttribute('href', 'dev-finances')
-            break
-        case 1:
-            projectTitle.innerText = "fastapping"
-            projectContent.innerText = "fastapping: este foi um projeto que eu criei do zero. Ele é um 'game' que testa quantas palavras o usuário consegue digitar por minuto e a precisão ao digitar e, conforme o usuário vai treinando, um gráfico vai sendo desenvolvido com a evolução dele. O programa foi feito com apenas HTML, CSS e Javascript puro. (foi feito para computador, portanto, não pode ser executado em celulares)."
-            project3.classList.remove("selected-li")
-            project2.classList.add("selected-li")
-            projectLink.setAttribute('href', 'fastapping')
-            break 
-        case 2:
-            projectTitle.innerText = "rocket.q"
-            projectContent.innerText = "rocket.q: este foi um projeto desenvolvido durante a NLW - together, da Rocketseat. Ele trata de ciar ou entrar em salas já existentes para fazer perguntas anônimas, que podem ser moderadas pelo criador da sala ou qualquer um que tenha a senha dela. O programa foi feito com EJS, Javascript, Node com express e usando o SQLite como banco de dados."
-            project4.classList.remove("selected-li")
-            project3.classList.add("selected-li")
-            projectLink.setAttribute('href', 'https://rocketq-app.herokuapp.com/')
-            break
-        case 3: 
-            projectTitle.innerText = "todo-list"
-            projectContent.innerText = "todo-list: este foi um projeto desenvolvido por mim para auxiliar os alunos do IFCE a iniciarem com o desenvolvimento para a web. Ele é um CRUD de uma lista de tarefas que possui o armazenamento local e permite criar quantas tarefas forem necessárias, marcá-las como concluídas e excluí-las. Ele usa somente HTML, CSS e Javascript puro."
-            project1.classList.remove("selected-li")
-            project4.classList.add("selected-li")
-            projectLink.setAttribute('href', 'todo-list')
-            break
-    }
+  switch (currentImageIndex) {
+    case 0:
+      projectTitle.innerText = devFinances.name
+      projectContent.innerText = devFinances.description
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project1.classList.add("selected-li")
+      projectLink.setAttribute('href', devFinances.link)
+      break
+    case 1:
+      projectTitle.innerText = fastapping.name
+      projectContent.innerText = fastapping.description
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project2.classList.add("selected-li")
+      projectLink.setAttribute('href', fastapping.link)
+      break
+    case 2:
+      projectTitle.innerText = rocketQ.name;
+      projectContent.innerText = rocketQ.description;
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project3.classList.add("selected-li")
+      projectLink.setAttribute('href', rocketQ.link);
+      break
+    case 3:
+      projectTitle.innerText = todoList.name;
+      projectContent.innerText = todoList.description;
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project4.classList.add("selected-li")
+      projectLink.setAttribute('href', todoList.link);
+      break
+    case 4:
+      projectTitle.innerText = pixelizeMe.name;
+      projectContent.innerText = pixelizeMe.description;
+      allProjectItems.forEach(projectItem => {
+        projectItem.classList.remove("selected-li")
+      })
+      project5.classList.add("selected-li")
+      projectLink.setAttribute('href', pixelizeMe.link);
+      break
+
+  }
 })
 
 function markSection(scrollDistance) {
-    allSideItems.forEach(sideItem => {
-        sideItem.classList.remove("selected-section")
-    })
+  allSideItems.forEach(sideItem => {
+    sideItem.classList.remove("selected-section")
+  })
 
-    if(scrollDistance == 0) {
-        document.getElementById("ref-section0").classList.add("selected-section")
-    } else if (scrollDistance >= height && scrollDistance < height * 2) {
-        document.getElementById("ref-section1").classList.add("selected-section")
-    } else if (scrollDistance >= height * 2 && scrollDistance < height * 3) {
-        document.getElementById("ref-section2").classList.add("selected-section")
-    } else if (scrollDistance >= height * 3) {
-        document.getElementById("ref-section3").classList.add("selected-section")
-    }
+  if (scrollDistance == 0) {
+    document.getElementById("ref-section0").classList.add("selected-section")
+  } else if (scrollDistance >= height && scrollDistance < height * 2) {
+    document.getElementById("ref-section1").classList.add("selected-section")
+  } else if (scrollDistance >= height * 2 && scrollDistance < height * 3) {
+    document.getElementById("ref-section2").classList.add("selected-section")
+  } else if (scrollDistance >= height * 3) {
+    document.getElementById("ref-section3").classList.add("selected-section")
+  }
 }
 
-function changeSideSelectedSection () {
-    setInterval(() => {
-        markSection(window.scrollY)
-    }, 500);
+function changeSideSelectedSection() {
+  setInterval(() => {
+    markSection(window.scrollY)
+  }, 500);
 }
 
 function isOnTop() {
-    if(window.scrollY === 0) {
-        return true;
-    } else {
-        return false;
-    }
+  if (window.scrollY === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 window.addEventListener("scroll", () => {
-    if(isOnTop() === true) {
-        scrollToTopButton.classList.remove("shownScrollTopButton")
-    } else {
-        scrollToTopButton.classList.add("shownScrollTopButton")
-    }
+  if (isOnTop() === true) {
+    scrollToTopButton.classList.remove("shownScrollTopButton")
+  } else {
+    scrollToTopButton.classList.add("shownScrollTopButton")
+  }
 })
 
 function scrollToTop() {
-    window.scrollTo(0, 0)
+  window.scrollTo(0, 0)
 }
 
 window.addEventListener("load", changeSideSelectedSection)
